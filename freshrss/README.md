eb80cc6f5d82bb4d3713395ddd939e8806ec70cc
Christian Schneider <christian-schneider@online.de>
2024-09-14 15:46:11 +0200

    added docker-compose.yml and backup script for freshrss

---
+++ freshrss/README.md
@@ -0,0 +1,5 @@
+# freshrss
+
+## backup
+
+When using the backup script with a docker container, the script does a full backup (see here [Full-Installation Backup](https://freshrss.github.io/FreshRSS/en/admins/05_Backup.html#full-installation-backup). The script will completly backup the /var/www/FreshRSS directory.
---
+++ freshrss/backup-freshrss.sh
@@ -0,0 +1,27 @@
+if [[ $# -ne 1 ]] ; then 
+	echo "Usage:"
+	echo "backup-freshrss.sh <container_name>"
+	echo "e.g. backup-freshrss.sh freshrss"
+	exit 0
+fi
+
+container_name=$1
+library_paths=$(echo $2 | tr ";" "\n")
+backup_file="$container_name-backup-$(date +"%d%m%Y-%H%M%S").tar.gz"
+
+echo -n "Stopping docker container: "
+docker container stop $container_name
+echo "Done."
+
+docker cp $container_name:/var/www/FreshRSS .
+
+tar -czf $backup_file FreshRSS
+
+rm -rf FreshRSS
+
+echo "Backup done "
+
+echo -n "Starting docker contaier: "
+docker container start $container_name
+echo "Done."
+
